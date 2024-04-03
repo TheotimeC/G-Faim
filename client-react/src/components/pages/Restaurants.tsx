1a1711b5c63e6d7d58dc9a6af7a6649645186d86
nboutika <boutikarnassim@gmail.com>
2024-04-03 09:38:31 +0200

    refactor(front): clean Restaurants component

--- client-react/src/components/pages/Restaurants.tsx
+++ client-react/src/components/pages/Restaurants.tsx
@@ -19,27 +19,7 @@ const Restaurants = () =>{
         <div>
             <h1>Restaurants</h1>
 
-            <Button text="Voir Menu" color="298029" size="10" onClick={function (): void {
-                throw new Error('Function not implemented.');
-            } } />
-            <Button text="Commander" color="FFA500" size="10" onClick={function (): void {
-                throw new Error('Function not implemented.');
-            } } />
-            <DefaultButton text="Accepter" textColor="FFFFFF" bgColor="298029" textSize="1rem" width="150px" marginLeft="10px" marginRight="10px" onClick={function (): void {
-                throw new Error('Function not implemented.');
-            } }/>
-            <DefaultButton text="Refuser" textColor="FFFFFF" bgColor="FF3A44" textSize="1rem" width="150px" marginLeft="10px" marginRight="10px" onClick={function (): void {
-                throw new Error('Function not implemented.');
-            } }/>
-            <DefaultButton text="Annuler" textColor="000000" bgColor="9A9BA1" textSize="1rem" width="150px" marginLeft="10px" marginRight="10px" onClick={function (): void {
-                throw new Error('Function not implemented.');
-            } }/>
-            <DefaultButton text="Message" textColor="FFFFFF" bgColor="FFA500" textSize="1rem" width="150px" marginLeft="10px" marginRight="10px" onClick={function (): void {
-                throw new Error('Function not implemented.');
-            } }/>
 
-
-            <Footer backgroundColor="D9D9D9" /*onSubscribe={handleSubscribe}*/ />
         </div>
     );
 }
