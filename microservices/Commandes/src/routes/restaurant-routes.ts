4b1b51da8e371833a5a248e0baf17278e23ba6be
nboutika <boutikarnassim@gmail.com>
2024-04-08 15:48:17 +0200

    feat(back): set restaurant order status

--- microservices/Commandes/src/routes/restaurant-routes.ts
+++ microservices/Commandes/src/routes/restaurant-routes.ts
@@ -2,13 +2,13 @@ import {FastifyInstance} from "fastify";
 import * as orderController from "../controllers/controllers";
 
 export default async function (fastify: FastifyInstance) {
-    fastify.get('/', {
-        handler: orderController.getOrderByRestaurantId,
-    });
     fastify.get('/current', {
         handler: orderController.getCurrentOrderByRestaurantId,
     });
     fastify.put('/status', {
-        handler: orderController.getCurrentOrderByRestaurantId,
+        handler: orderController.setOrderRestaurantStatus,
+    });
+    fastify.get('/', {
+        handler: orderController.getOrderByRestaurantId,
     });
 }
