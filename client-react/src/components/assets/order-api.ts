69c935aa36aefff56f5bd5ed5a17f752a8471ee6
nboutika <boutikarnassim@gmail.com>
2024-04-10 08:25:01 +0200

    refactor(front): modified order-api enum to match back + added getOrdersByUserId function

--- client-react/src/components/assets/order-api.ts
+++ client-react/src/components/assets/order-api.ts
@@ -1,18 +1,23 @@
 import api from "./api.ts"
-import axios from "axios";
 
 const baseRoute = "http://localhost:3002/api"
-export enum RestaurantStatus {
-    TO_ACCEPT = "to accept",
-    IN_PREPARATION = "in preparation",
-    READY = "ready",
+export enum OrderStatus {
+    CANCELLED = "Annulée",
+    CART = "Panier",
+    PAID = "Payée",
+    TO_ACCEPT = "A accepter",
+    IN_PREPARATION = "En préparation",
+    AWAITING_PICKUP = "En attente de retrait",
+    IN_TRANSIT = "En cours de livraison",
+    DELIVERED = "Livrée",
 }
+
 // Define the API interface for communicating with the backend
 const orderApi = {
     // User Routes
     getUserCart: (userId: string) => api.get(baseRoute + `/users/cart?userId=${userId}`),
     updateCart: (userId: string, cartData: any) => api.put(baseRoute + `/users/cart?userId=${userId}`, cartData),
-    itemId: (userId: string, restaurantId: string, item: any) => axios.put(baseRoute + `/users/cart/create?userId=${userId}`, {restaurantId, item} ),
+    itemId: (userId: string, restaurantId: string, item: any) => api.put(baseRoute + `/users/cart/create?userId=${userId}`, {restaurantId, item} ),
     addItemToCart: (userId: string, itemData: any) => api.put(baseRoute + `/users/cart/item?userId=${userId}`, itemData),
     deleteCart: (userId: string) => api.delete(baseRoute + `/users/cart?userId=${userId}`),
     deleteCartItem: (userId: string, itemId: string) => api.delete(baseRoute + `/users/cart/item?userId=${userId}&itemId=${itemId}`),
@@ -21,6 +26,10 @@ const orderApi = {
     getOrdersByRestaurantId: (restaurantId: string) => api.get(baseRoute + `/restaurants?restaurantId=${restaurantId}`),
     getCurrentOrdersByRestaurantId: (restaurantId: string) => api.get(baseRoute + `/restaurants/current?restaurantId=${restaurantId}`),
     setRestaurantStatus: (orderId: string, restaurantStatus: any) => api.put(`${baseRoute}/restaurants/status?id=${orderId}`, { restaurantStatus }),
+
+    //Users routes
+    getOrdersByUserId: (userId: string) => api.get(baseRoute + `/users?userId=${userId}`),
+
 
     // Basic Routes
     createOrder: (orderData: any) => api.post(baseRoute + '/orders', orderData),
