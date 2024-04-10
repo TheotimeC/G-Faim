534dac69ee2c46dd624c23833219b1725672b361
nboutika <boutikarnassim@gmail.com>
2024-04-10 08:18:41 +0200

    feat(front): added "user-api.ts" for interface between front and back

---
+++ client-react/src/components/assets/user-api.ts
@@ -0,0 +1,9 @@
+import api from "./api.ts";
+const API_URL = 'http://localhost:3000/user';
+export const getUserId = async () => {
+    const token = localStorage.getItem('accessToken')
+    const response = await api.get(`${API_URL}/getId/`, {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+    return response.data._id;
+}
